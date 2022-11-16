270265343fa76c4a946aabe4a78bb5138311e419
talanchic <katyuha_talanova@mail.ru>
2022-11-16 15:29:37 +0500

    beginning of the homework

---
+++ PhoneBook/input.py
@@ -0,0 +1,9 @@
+def get_info ():
+    info = []
+    name = input('Введите имя: ')
+    info.append(name)
+    phone = input('Введите телефон: ')
+    info.append(phone)
+    about = input('Введите описание: ')
+    info.append(about)
+    return info
